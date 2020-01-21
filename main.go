303701511be6393644d94d89d9c0bff37a6cb5e6
siddharthr <siddharthr@qubole.com>
2020-01-21 14:36:46 +0530

    changes for bird handler

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 // The new router function creates the router and
@@ -13,6 +12,26 @@ import (
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the
+	// directory we just made
+	staticFileDirectory := http.Dir("./assets/")
+
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/assets/" prefix when looking for files.
+	// For example, if we type "/assets/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for
+	// "./assets/assets/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/birds/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/birds").Handler(staticFileHandler).Methods("GET")
+
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+
 	return r
 }
 
--- main_test.go
+++ main_test.go
@@ -46,7 +46,7 @@ func TestRouter(t *testing.T) {
 	// If it does happen to be "Hello world!", then it confirms, that the
 	// route is correct
 	if respString != expected {
-	t.Errorf("Response should be %s, got %s", expected, respString)
+		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
 
 }
@@ -82,3 +82,31 @@ func TestRouterForNonExistentRoute(t *testing.T) {
 	}
 
 }
+
+func TestStaticFileServer(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	// We want to hit the `GET /assets/` route to get the index.html file response
+	resp, err := http.Get(mockServer.URL + "/birds/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We want our status to be 200 (ok)
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	}
+
+	// It isn't wise to test the entire content of the HTML file.
+	// Instead, we test that the content-type header is "text/html; charset=utf-8"
+	// so that we know that an html file has been served
+	contentType := resp.Header.Get("Content-Type")
+	expectedContentType := "text/html; charset=utf-8"
+
+	if expectedContentType != contentType {
+		t.Errorf("Wrong content type, expected %s, got %s", expectedContentType, contentType)
+	}
+
+}
+
